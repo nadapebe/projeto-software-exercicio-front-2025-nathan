2a7d47100af31a828027036dac44304787bb402d
nadapebe <nathandpb@al.insper.edu.br>
2025-11-10 11:18:27 -0300

    alsa

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './App.css';
-import CursosApp from './Curso';
+import CursosApp from './Curso'
 
 export default function App() {
   return <CursosApp />;
