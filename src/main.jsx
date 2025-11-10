b218ac357f0474f4a4a2a1dea9536da62cce8da0
nadapebe <nathandpb@al.insper.edu.br>
2025-11-10 11:24:20 -0300

    vai

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Auth0Provider } from "@auth0/auth0-react";
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <Auth0Provider
     domain="dev-2apkktmjitillc05.us.auth0.com"
     clientId="1d7KEc71I9mQaEKydfOtVi454L3cjkuB"
