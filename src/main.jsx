363a8eebc72304425ef617a18cda655949b1d470
Nathan Benaion <159028802+nadapebe@users.noreply.github.com>
2025-11-09 23:49:40 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
-      domain="dev-vlbhbshl7b1pxe8e.us.auth0.com"
-      clientId="4rwe12yYJbxAD122EDkjl61unHBaHFL0"
+      domain="dev-2apkktmjitillc05.us.auth0.com"
+      clientId="1d7KEc71I9mQaEKydfOtVi454L3cjkuB"
       authorizationParams={{
-        audience: "https://dev-vlbhbshl7b1pxe8e.us.auth0.com/api/v2/",
+        audience: "https://dev-2apkktmjitillc05.us.auth0.com/api/v2/",
         redirect_uri: window.location.origin
       }}
     >
