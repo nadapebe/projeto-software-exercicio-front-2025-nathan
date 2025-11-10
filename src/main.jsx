0f06c84bfc285b3e3c3329323654810e5e55ca19
nadapebe <nathandpb@al.insper.edu.br>
2025-11-10 11:39:21 -0300

    vai3

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
-import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
-import App from './App.jsx';
-import './index.css';
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react'
+
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
-    domain="dev-2apkktmjitillc05.us.auth0.com"
-    clientId="1d7KEc71I9mQaEKydfOtVi454L3cjkuB"
-    authorizationParams={{
-      audience: "https://curso-api",
-      redirect_uri: window.location.origin, // volta para /
-    }}
-    cacheLocation="localstorage"   // <— importante para evitar loop
-    useRefreshTokens               // <— importante para evitar loop
-  >
+      domain="dev-2apkktmjitillc05.us.auth0.com"
+      clientId="1d7KEc71I9mQaEKydfOtVi454L3cjkuB"
+      authorizationParams={{
+        audience: "https://dev-2apkktmjitillc05.us.auth0.com/api/v2/",
+        redirect_uri: window.location.origin
+      }}
+    >
     <App />
-  </Auth0Provider>
-);
+  </Auth0Provider>,
+)
